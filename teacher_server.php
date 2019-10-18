9fc9c49a91f053ab2ea891fa7a269a9ffcc2775c
Ajayshiva-1310 <ajayshiva.p.2017.cse@rajalakshmi.edu.in>
2019-10-18 16:47:36 +0530

    Dynamic period count setting

--- teacher_server.php
+++ teacher_server.php
@@ -9,11 +9,22 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 $sub=$_SESSION['id'];
-$arr=['ip'=>['Mon'=>0,'Tue'=>0,'Wed'=>2,'Thu'=>0,'Fri'=>0,'Sat'=>2,'Sun'=>3],'cns'=>['Mon'=>1,'Tue'=>1,'Wed'=>1,'Thu'=>0,'Fri'=>2,'Sat'=>0]];
-$a=date('D');
+#$arr=['ip'=>['Mon'=>0,'Tue'=>0,'Wed'=>2,'Thu'=>0,'Fri'=>0,'Sat'=>2,'Sun'=>3],'cns'=>['Mon'=>1,'Tue'=>1,'Wed'=>1,'Thu'=>0,'Fri'=>2,'Sat'=>0]];
+$v=date('l');
+echo $v;
+$q="Select " .$v. " from period where name LIKE '$sub'";
+#echo $q;
+#echo "<br>";
+$result=$conn->query($q);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $val = $row[$v] ;
+    }
+  }
 echo "<script>console.log(".$_POST['button'].")</script>";
 echo "<script>console.log(".$_SESSION['id'].")</script>";
-$val=$arr[$sub][$a];
+#$val=$arr[$sub][$a];
 $buttons = array('1','2','3','4','5','6','7','8','9','10'); 
 foreach ($buttons as $button) { 
         if (($_POST['button'])==$button) {
