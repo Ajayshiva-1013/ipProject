569d540e0a8a7e50d60bbe1f65c7f60fd4d20eb9
BharathA14 <bbharath201@gmail.com>
2019-10-20 15:02:16 +0530

    addstudent backend

--- addstudent.php
+++ addstudent.php
@@ -1,10 +1,10 @@
 <?php
+
 #get these values using ajax, i have used static values just to check whether the query is working.
-$id=11;
-$pass="pass";
-$name="Aishwarya R";
- 
-  session_start();
+$id=$_POST['id'];
+$pass=$_POST['pass'];
+$name=$_POST['name'];
+echo '<script>alert("worling");</script>';
   $servername = "localhost";
   $username = "ajay";
   $password = "";
@@ -13,7 +13,7 @@ $name="Aishwarya R";
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-$q="INSERT INTO stu_login (id , pwd , namee ) VALUES ($id,'$pass','$name')";
+$q="INSERT INTO stu_login (id, pwd , namee ) VALUES ('$id','$pass','$name')";
 if ($conn->query($q) === TRUE) {
     echo "New record created successfully";
 } else {
