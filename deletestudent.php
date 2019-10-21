da0827ef9ec4f5ebf75339ab55811e696ded6980
Ajayshiva-1310 <ajayshiva.p.2017.cse@rajalakshmi.edu.in>
2019-10-21 09:17:14 +0530

    delete student completed.

--- deletestudent.php
+++ deletestudent.php
@@ -1,6 +1,6 @@
 <?php
 #get these values using ajax, i have used static values just to check whether the query is working.
-  $name=$_POST['name'];
+  $name='Aakash P';
   $servername = "localhost";
   $username = "ajay";
   $password = "";
@@ -9,12 +9,29 @@
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-$q="DELETE FROM stu_login where namee = '$name' ";
-echo $q;
-if ($conn->query($q) === TRUE) {
+  $q="Select id from stu_login where namee = '$name'";
+#echo $q;
+#echo "<br>";
+$result=$conn->query($q);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $val = $row['id'] ;
+        echo $val;
+    }
+  }
+$q1="DELETE FROM stu_login where namee = '$name' ";
+//echo $q;
+if ($conn->query($q1) === TRUE) {
     echo "Record deleted successfully";
 } else {
-    echo "Error: " . $q . "<br>" . $conn->error;
+    echo "Error: " . $q1 . "<br>" . $conn->error;
 }
-$conn->close();
+$q3="Delete from attendance where id = ".$val;
+if ($conn->query($q3) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error: " . $q3 . "<br>" . $conn->error;
+}
+
 ?>
